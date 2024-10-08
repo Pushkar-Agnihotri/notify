2405d62cbd3ea53e0bd713602a75f96670992522
Pushkar Agnihotri <pushkar.agnihotri@pushkar.agnihotri>
2024-10-08 12:52:41 +0530

    env variables

--- email_adapter.py
+++ email_adapter.py
@@ -2,6 +2,10 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 class EmailAdapter:
     def send(self, message, to_email, subject="Notification"):
